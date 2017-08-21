94686b4856ec7b4e6d789f782443f2a1d939a483
admin <admin@FASTDRAG>
2017-08-21 13:38:35 +0300

    Bug with multi listeners

--- SimpleEventBus/src/main/java/ru/sendto/gwt/client/util/Bus.java
+++ SimpleEventBus/src/main/java/ru/sendto/gwt/client/util/Bus.java
@@ -41,32 +41,27 @@ public class Bus {
 			return list==null?new ArrayList<Bus.Event>():list;
 		}
 		
-		public <A,R> void listen(Class<A> key, Event<A,R> value) {
+		public <A,R> void wire(Class<A> key, Event<A,R> value) {
 			List<Event> list = get(key);
 			list.add(value);
 			put(key, list);
 		}
 		
-		public <A> void listen(Class<A> key, VoidEvent<A> value) {
+		public <A> void wire(Class<A> key, VoidEvent<A> value) {
 			List<Event> list = get(key);
 			list.add(t->{value.invoke((A)t);return null;});
 			put(key, list);
 		}
 		
 		public <A,R> List<R>  fire (A o){
-			
-			Log.console("buss count "+busMap.size());
-			
+
 			Class<A> clz = (Class<A>) o.getClass();
-			
 			List tmp =  get(clz);
 			List<Event<A,R>> list =tmp;
 			
-			Log.console("listeners list size "+list.size());
-			
 			Object[] results = new Object[list.size()];
 			for (int i=0 ; i< list.size(); i++){//Event<A, R> event : list) {
-				results[i]=list.get(0).invoke(o);
+				results[i]=list.get(i).invoke(o);
 			}
 			
 			return (List<R>) Arrays.asList(results);
@@ -89,7 +84,7 @@ public class Bus {
 	 * @param <R> - event listener
 	 */
 	public <A,R> void listen(Class<A> key, Event<A,R> value) {
-		map.listen(key, value);
+		map.wire(key, value);
 	}
 	/**
 	 * Add void listener
@@ -98,10 +93,9 @@ public class Bus {
 	 * @param <A> - event listener
 	 */
 	public <A> void listen(Class<A> key, VoidEvent<A> value) {
-		map.listen(key, value);
+		map.wire(key, value);
 	}
-	public <A,R> List<R> 
-		fire (A o){
+	public <A,R> List<R> fire (A o){
 		return map.fire(o);
 	}
 
@@ -117,7 +111,7 @@ public class Bus {
 	 * @param busName bus name
 	 * @return event bus
 	 */
-	static public Bus get(String busName){
+	static public Bus getBy(String busName){
 		if(busName==null)
 			return get();
 		
@@ -133,22 +127,22 @@ public class Bus {
 	 * @param busName bus name
 	 * @return event bus
 	 */
-	static public Bus get(Class<?> busName){
+	static public Bus getBy(Class<?> busName){
 		if(busName==null)
 			return get();
 		
-		return get(busName.getName());
+		return getBy(busName.getName());
 	}
 	/**
 	 * Get bus instance by object class name
 	 * @param busName bus name
 	 * @return event bus
 	 */
-	static public Bus get(Object busName){
+	static public Bus getBy(Object busName){
 		if(busName==null)
 			return get();
 			
-		return get(busName.getClass());
+		return getBy(busName.getClass());
 	}
 	
 }
